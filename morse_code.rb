4aa84c7ebd4ce03a13b8c9aad6f8eb331ddb6128
vikipretium <vighneshpasupathy@gmail.com>
2022-05-13 20:41:41 +0530

    fix linters

--- morse_code.rb
+++ morse_code.rb
@@ -1,43 +1,22 @@
 def morse_code(code)
-    codes = {
-        '.-' => 'A',
-        '-...' => 'B',
-        '-.-.' => 'C',
-        '-..' => 'D',
-        '.' => 'E',
-        '..-.' => 'F',
-        '--.' => 'G',
-        '....' => 'H',
-        '..' => 'I',
-        '.---' => 'J',
-        '-.-' => 'K',
-        '.-..' => 'L',
-        '--' => 'M',
-        '-.' => 'N',
-        '---' => 'O',
-        '.--.' => 'P',
-        '--.-' => 'Q',
-        '.-.' => 'R',
-        '...' => 'S',
-        '-' => 'T',
-        '..-' => 'U',
-        '...-' => 'V',
-        '.--' => 'W',
-        '-..-' => 'X',
-        '-.--' => 'Y',
-        '--..' => 'Z'
-      }
-      codes[code]
-    end
+  codes = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
+    '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V',
+    '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z'
+  }
+  codes[code]
+end
 
 def full_decode(full_encryption)
   full_encryption = full_encryption.split('   ')
-  full_encryption.map {|word| decode(word) }.join(" ")
+  full_encryption.map { |word| decode(word) }.join(' ')
 end
 
 def decode(encryption)
   letters = encryption.split
-  letters.map{ |letter| morse_code(letter)}.join
+  letters.map { |letter| morse_code(letter) }.join
 end
 
-print full_decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+print full_decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
